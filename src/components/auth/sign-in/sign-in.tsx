98202669e6307d94f7482dfd88d601a63fe934c0
AlexMadM <alex.alexandrov.1988@gmail.com>
2025-01-06 21:07:37 +0300

    signIn

---
+++ src/components/auth/sign-in/sign-in.tsx
@@ -0,0 +1,14 @@
+import {Card} from "../../ui";
+
+
+const SignIn = () => {
+    return (
+        <Card className='bg-dark-700 rounded-sm w-[420px]'>
+<div className='w-350 flex flex-col items-center '>
+
+</div>
+        </Card>
+    );
+};
+
+export default SignIn;
