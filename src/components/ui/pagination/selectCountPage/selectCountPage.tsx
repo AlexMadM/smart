4f233f1179fb04e64291f7b1c4adcba1f15fd5f0
AlexMadM <alex.alexandrov.1988@gmail.com>
2025-02-28 18:11:57 +0300

    select

--- src/components/ui/pagination/selectCountPage/selectCountPage.tsx
+++ src/components/ui/pagination/selectCountPage/selectCountPage.tsx
@@ -1,26 +1,26 @@
-import s from '../pagination.module.scss'
-import { Select } from '../../select'
-
-type SelectCountPageProps = {
-  placeholder: number
-  setPageSize: (value: number) => void
-}
-export const SelectCountPage = ({ placeholder, setPageSize }: SelectCountPageProps) => {
-  const selectCountPageHandler = (value: string) => {
-    setPageSize(+value)
-  }
-
-  return (
-    <div className={s.selectContainer}>
-      Показать
-      <Select
-        className={s.select}
-        onValueChange={selectCountPageHandler}
-        options={['5', '10', '20', '30', '50']}
-        placeholder={placeholder}
-        // value={'5'}
-      />
-      на странице
-    </div>
-  )
-}
+// import s from '../pagination.module.scss'
+// import { Select } from '../../select'
+//
+// type SelectCountPageProps = {
+//   placeholder: number
+//   setPageSize: (value: number) => void
+// }
+// export const SelectCountPage = ({ placeholder, setPageSize }: SelectCountPageProps) => {
+//   const selectCountPageHandler = (value: string) => {
+//     setPageSize(+value)
+//   }
+//
+//   return (
+//     <div className={s.selectContainer}>
+//       Показать
+//       <Select
+//         className={s.select}
+//         onValueChange={selectCountPageHandler}
+//         options={[5, 10, 20, 30, 50]}
+//         placeholder={ placeholder}
+//         value={'5'}
+//       />
+//       на странице
+//     </div>
+//   )
+// }
